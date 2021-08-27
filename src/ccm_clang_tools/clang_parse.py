d7fa5d1d85455b71196ae6181e3d28cb2074df4e
Gabriel Ingram <gabriel.ingram@colorado.edu>
2021-08-27 02:25:43 -0600

    Add include paths options, add prettify & recursion options

--- src/ccm_clang_tools/clang_parse.py
+++ src/ccm_clang_tools/clang_parse.py
@@ -1,16 +1,27 @@
 import os
 import argparse
 import subprocess
+import pdb
 
 from .utils import (
     clang_tool_path
 )
 
-def command(files, file_base, out_dir, clang_tool_verbose, 
-        plugin_loc, plugin_name, clang, force_out_in_src):
+def command(
+        files,
+        include_paths,
+        file_base,
+        out_dir,
+        clang_tool_verbose, 
+        plugin_loc,
+        plugin_name,
+        recursion_level, 
+        clang,
+        force_out_in_src,
+        prettify):
 
     for _file in files:
-   
+  
         file_dirname = os.path.dirname(os.path.join(file_base, _file))
         out_dirname = out_dir if out_dir else file_dirname
 
@@ -26,13 +37,22 @@ def command(files, file_base, out_dir, clang_tool_verbose,
         else:
             out_filename = os.path.join(out_dirname, out_filename)
 
+        include = ""
+        for inc_path in include_paths:
+            include += f" -Xclang -I{inc_path}"
+
         inv = "clang -fsyntax-only -Xpreprocessor -detailed-preprocessing-record"
+        inv += include
         for clang_arg in clang:
             inv += f" -Xclang {clang_arg}"
         inv += " -Xclang -load"
         inv += f" -Xclang {os.path.join(plugin_loc, plugin_name)}"
         inv += " -Xclang -plugin"
         inv += " -Xclang JsonASTExporter"
+        inv += " -Xclang -plugin-arg-JsonASTExporter"
+        inv += f" -Xclang RECURSION_LEVEL={recursion_level}"
+        inv += " -Xclang -plugin-arg-JsonASTExporter"
+        inv += f" -Xclang PRETTIFY_JSON={int(prettify)}"
         inv += " -Xclang -plugin-arg-JsonASTExporter"
         inv += f" -Xclang {out_filename} -c {os.path.join(file_base, _file)}"
 
@@ -44,11 +64,20 @@ def command(files, file_base, out_dir, clang_tool_verbose,
 
         stream = os.popen(inv)
         out = stream.read()
-        print(out)
+        if out:
+            print(out)
 
-        return
+    return
 
-def docker_command(files, file_base, out_dir, clang_tool_verbose, clang, force_out_in_src):
+def docker_command(
+        files,
+        include_paths,
+        file_base,
+        out_dir,
+        clang_tool_verbose,
+        recursion_level,
+        clang,
+        force_out_in_src):
 
     call_dir = os.getcwd()
     inv = ""
@@ -57,6 +86,10 @@ def docker_command(files, file_base, out_dir, clang_tool_verbose, clang, force_o
 
     mt_in = f" -v {file_base}:/src"
     inv += " --file-base /src"
+
+    mt_includes = ""
+    for inc_path in include_paths:
+        mt_includes += " -v {inc_path}:{inc_path}"
 
     inv += " --files"
     for _file in files:
@@ -73,8 +106,18 @@ def docker_command(files, file_base, out_dir, clang_tool_verbose, clang, force_o
     if clang_tool_verbose:
         inv += " --clang-tool-verbose"
 
+    if recursion_level == 0 or recursion_level is None:
+        inv += " --no-recursion"
+    elif recursion_level == 1:
+        inv += " --recurse-inheritance"
+    elif recursion_level == 2:
+        inv += " --recurse-all"
+
     if force_out_in_src:
         inv += " --force-out-in-src"
+
+    if prettify:
+        inv += " --prettify"
 
     for clang_arg in clang:
         inv += f" {clang_arg}"
@@ -82,12 +125,14 @@ def docker_command(files, file_base, out_dir, clang_tool_verbose, clang, force_o
     docker_inv = "docker run -it"
     docker_inv += f" {mt_in}"
     docker_inv += f" {mt_out}"
+    docker_inv += f" {mt_includes}"
     docker_inv += " gjingram/ccm-clang-tools:latest"
     docker_inv += f" {inv}"
 
     stream = os.popen(docker_inv)
     out = stream.read()
-    print(out)
+    if out:
+        print(out)
 
     return
 
@@ -102,6 +147,13 @@ def run_clang_parse():
             default=False
             )
     aparse.add_argument(
+            "--prettify",
+            "-p",
+            help="Prettify JSON output",
+            action="store_true",
+            default=False
+            )
+    aparse.add_argument(
             "--file-base",
             help="Interpret file name as being relative to this",
             default=os.getcwd()
@@ -112,6 +164,11 @@ def run_clang_parse():
             nargs="+",
             default=None
            )
+    aparse.add_argument(
+            "-I",
+            "--includes",
+            nargs="?"
+            )
     aparse.add_argument(
             "--out-dir",
             help="Parse JSON out directory",
@@ -145,31 +202,64 @@ def run_clang_parse():
             action="store_true",
             default=False
             )
+    aparse.add_argument(
+            "--no-recursion",
+            help="Don't recurse into any referenced declarations",
+            action="store_true"
+            )
+    aparse.add_argument(
+            "--recurse-inherited",
+            help="Recurse into declarations referenced via class inheritance",
+            action="store_true"
+            )
+    aparse.add_argument(
+            "--recurse-all",
+            help="Recurse into all declarations & types referenced",
+            action="store_true"
+            )
 
 
     known, unknown = aparse.parse_known_args()
     if not known.files or len(known.files) == 0:
         raise RuntimeError("No input files provided")
 
+    recursion_level = 0
+    if known.recurse_inherited:
+        recursion_level = 1
+    if known.recurse_all:
+        recursion_level = 2
+    if known.no_recursion:
+        recursion_level = 0
+
+    includes = []
+    if known.includes:
+        includes = known.includes
+
     if known.docker:
         docker_command(
                 known.files,
+                includes,
                 known.file_base,
                 known.out_dir,
                 known.clang_tool_verbose,
+                recursion_level,
                 unknown,
-                known.force_out_in_src
+                known.force_out_in_src,
+                known.prettify
                 )
     else:
         command(
                 known.files,
+                includes,
                 known.file_base,
                 known.out_dir,
                 known.clang_tool_verbose,
                 known.plugin_loc,
                 known.plugin_name,
+                recursion_level,
                 unknown,
-                known.force_out_in_src
+                known.force_out_in_src,
+                known.prettify
                 )
 
     return
